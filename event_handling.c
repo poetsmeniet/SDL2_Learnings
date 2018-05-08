3d04cf8666e49ac8782f6c74afe35be3d5997d40
Thomas <thomas@geenbs.nl>
2018-05-08 13:42:30 +0200

    Initial commit

---
+++ event_handling.c
@@ -0,0 +1,102 @@
+/* compile: gcc -lSDL2 -lSDL2_image */
+
+#include <SDL2/SDL.h>
+#include <stdio.h>
+#include <SDL2/SDL_image.h>
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+
+SDL_Window *init();
+void loadImage(SDL_Window *window, char *file);
+void closeAll(SDL_Window *window);
+int handleEvents(SDL_Window *window);
+
+int main( int argc, char* args[] )
+{
+	SDL_Window* window = init();
+
+    while(handleEvents(window)){
+        SDL_Delay(5);
+    }
+
+    closeAll(window);
+	return 0;
+}
+
+/* Init window */
+SDL_Window * init()
+{
+    IMG_Init(IMG_INIT_JPG);
+
+	if( SDL_Init( SDL_INIT_VIDEO ) < 0 ){
+		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+        return NULL;
+	}
+	
+	SDL_Window *window = SDL_CreateWindow( "Template window", \
+            SDL_WINDOWPOS_UNDEFINED, \
+            SDL_WINDOWPOS_UNDEFINED, \
+            SCREEN_WIDTH, \
+            SCREEN_HEIGHT, \
+            SDL_WINDOW_SHOWN );
+
+	if( window == NULL ){
+		printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+        return NULL;
+	}
+
+    return window;
+}
+
+/* Loads an image onto surface */
+void loadImage(SDL_Window *window, char *file)
+{
+	SDL_Surface *screenSurface = SDL_GetWindowSurface( window );
+
+	SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
+
+    /* IMG_Load needs additional lib */
+    SDL_Surface *surfImg = IMG_Load(file);
+    if( surfImg == NULL ) { 
+        printf( "Unable to load image %s! SDL Error: %s\n", "cat2.jpg", SDL_GetError()); 
+    } 
+
+	SDL_BlitSurface( surfImg, NULL, screenSurface, NULL );
+	SDL_UpdateWindowSurface(window);
+}
+
+void closeAll(SDL_Window *window)
+{
+	SDL_DestroyWindow( window );
+    IMG_Quit();
+	SDL_Quit();
+}
+
+int handleEvents(SDL_Window *window)
+{
+    SDL_Event e;
+
+    while(SDL_PollEvent(&e) != 0){
+        /* Handle quit event */
+        if(e.type == SDL_QUIT)
+            return 0;
+
+        /* Handle keypresses */
+        if(e.type == SDL_KEYDOWN){
+            switch(e.key.keysym.sym){
+                case SDLK_UP:
+                    loadImage(window, "cat.bmp");
+                    break;
+                case SDLK_DOWN:
+                    loadImage(window, "cat2.jpg");
+                    break;
+                case SDLK_q:
+                    loadImage(window, "q.jpg");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    return 1;
+}

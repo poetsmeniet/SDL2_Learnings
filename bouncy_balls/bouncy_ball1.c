77b90cdfd5bbb0ee46b698723c81dcf1cf559450
Thomas <thomas@geenbs.nl>
2018-05-18 19:16:34 +0200

    Added more smooth movement

--- bouncy_balls/bouncy_ball1.c
+++ bouncy_balls/bouncy_ball1.c
@@ -1,4 +1,7 @@
-/* compile: gcc -lSDL2 -lSDL2_image */
+/* Implement bouncy ball with movement capabilities 
+ * I rewrote the C++ "SDL2 movement tutorial" from Lazyfoo
+ * to plain C
+ compile: gcc -lSDL2 -lSDL2_image */
 
 #include <SDL2/SDL.h>
 #include <stdio.h>
@@ -11,11 +14,14 @@ SDL_Window *init();
 SDL_Renderer *initRenderer(SDL_Window *window);
 SDL_Texture *loadImageAsTexture(SDL_Window *window, SDL_Renderer *gRenderer, char *file, int width, int height, int x, int y);
 void closeAll(SDL_Window *window);
-int handleEvents(SDL_Window *window, SDL_Renderer *gRenderer, SDL_Rect *ballRect);
-void bounce(SDL_Rect *ballRect);
+int handleEvents(SDL_Window *window, SDL_Renderer *gRenderer, SDL_Rect *bRect);
+void bounce(SDL_Rect *bRect);
+void move(SDL_Rect *bRect);
 
 int velocity = 10;
 int directionY = 0;
+int velX= 0;
+int velY= 0;
 
 int main(int argc, char* args[])
 {
@@ -23,33 +29,34 @@ int main(int argc, char* args[])
     SDL_Renderer *gRenderer = initRenderer(window);
     
     SDL_RendererFlip flip = SDL_FLIP_NONE;
-    SDL_Rect ballRect; 
-    ballRect.x = 0; 
-    ballRect.y = 0; 
-    ballRect.w = SCREEN_WIDTH; 
-    ballRect.h = SCREEN_HEIGHT; 
+    SDL_Rect bRect; 
+    bRect.x = 0; 
+    bRect.y = 0; 
+    bRect.w = SCREEN_WIDTH; 
+    bRect.h = SCREEN_HEIGHT; 
 
     /* Initial position of ball */
     int x = 0;
     int y = 0;
 
     /* Ball dimensions */
-    ballRect.w = 30; 
-    ballRect.h = 30; 
+    bRect.w = 30; 
+    bRect.h = 30; 
 
     /* Load background image */
     SDL_Texture *bg = loadImageAsTexture(window, gRenderer, "tdh_basketball_court_by_marcellsalek_26-d9aijwe.png", SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0);
     SDL_Texture *bball = loadImageAsTexture(window, gRenderer, "bbal_30.png", 30, 30, x, y);
         
     directionY = 1;
-    while(handleEvents(window, gRenderer, &ballRect)){
-        bounce(&ballRect);
+    while(handleEvents(window, gRenderer, &bRect)){
+        move(&bRect);
+        bounce(&bRect);
         SDL_RenderClear(gRenderer);
 
         /* Render bg */
         SDL_RenderCopyEx(gRenderer, bg, NULL, NULL, 0.0, NULL, flip);
     
-        int test = SDL_RenderCopyEx(gRenderer, bball, NULL, &ballRect, 0.0, NULL, flip);
+        int test = SDL_RenderCopyEx(gRenderer, bball, NULL, &bRect, 0.0, NULL, flip);
 	    SDL_RenderPresent(gRenderer);
     }
 
@@ -113,26 +120,19 @@ SDL_Texture *loadImageAsTexture(SDL_Window *window, SDL_Renderer *gRenderer, cha
         printf("Unable to load image %s! SDL Error: %s\n", "cat2.jpg", SDL_GetError()); 
     } 
 
-    /* Optimize surface */
-   // surfImg = SDL_ConvertSurface(surfImg, screenSurface->format, 0); 
-   // if(surfImg == NULL){ 
-   //     printf("Unable to optimize image %s! SDL Error: %s\n", file, SDL_GetError()); 
-   // }
-   
     /* Convert to texture */
     SDL_RendererFlip flip = SDL_FLIP_NONE;
-    SDL_Rect ballRect; 
-    ballRect.x = x; 
-    ballRect.y = y; 
-    ballRect.w = surfImg->w; 
-    ballRect.h = surfImg->h; 
+    SDL_Rect bRect; 
+    bRect.x = x; 
+    bRect.y = y; 
+    bRect.w = surfImg->w; 
+    bRect.h = surfImg->h; 
 
 	SDL_Texture* newTexture = NULL;
     newTexture = SDL_CreateTextureFromSurface(gRenderer, surfImg);
 	if(newTexture == NULL)
 		printf("Unable to create texture from %s! SDL Error: %s\n", file, SDL_GetError());
 
-	//SDL_UpdateWindowSurface(window);
     SDL_FreeSurface(surfImg);
 
     return newTexture;
@@ -145,7 +145,7 @@ void closeAll(SDL_Window *window)
 	SDL_Quit();
 }
 
-int handleEvents(SDL_Window *window, SDL_Renderer *gRenderer, SDL_Rect *ballRect)
+int handleEvents(SDL_Window *window, SDL_Renderer *gRenderer, SDL_Rect *bRect)
 {
     SDL_Event e;
 
@@ -155,19 +155,19 @@ int handleEvents(SDL_Window *window, SDL_Renderer *gRenderer, SDL_Rect *ballRect
             return 0;
 
         /* Handle keypresses */
-        if(e.type == SDL_KEYDOWN && e.key.repeat == 1){
+        if(e.type == SDL_KEYDOWN && e.key.repeat == 0){
             switch(e.key.keysym.sym){
                 case SDLK_UP:
-                    ballRect->y-=velocity;
+                    velY -= velocity;
                     break;
                 case SDLK_DOWN:
-                    ballRect->y+=velocity;
+                    velY += velocity;
                     break;
                 case SDLK_LEFT:
-                    ballRect->x-=velocity;
+                    velX -= velocity;
                     break;
                 case SDLK_RIGHT:
-                    ballRect->x+=velocity;
+                    velX += velocity;
                     break;
                 case SDLK_q:
                     return 0;
@@ -176,30 +176,68 @@ int handleEvents(SDL_Window *window, SDL_Renderer *gRenderer, SDL_Rect *ballRect
                     break;
             }
         }
+
+        if(e.type == SDL_KEYUP && e.key.repeat == 0){
+            switch(e.key.keysym.sym){
+                case SDLK_UP:
+                    velY += velocity;
+                    break;
+                case SDLK_DOWN:
+                    velY -= velocity;
+                    break;
+                case SDLK_LEFT:
+                    velX += velocity;
+                    break;
+                case SDLK_RIGHT:
+                    velX -= velocity;
+                    break;
+                default:
+                    break;
+            }
+        }
     }
+
     return 1;
 }
 
-void bounce(SDL_Rect *ballRect)
+/* Create bouncy effect */
+void bounce(SDL_Rect *bRect)
 {
-    //static int directionY = 10;
+    /* Define acceleration */
     static int ac = 0;
     
-    ballRect->y += (directionY + ac);
+    bRect->y += (directionY + ac);
     if(ac < 15)
         ac++;
 
     int floorOffset = 100;
 
     /* If the dot reaches top or bottom */
-    if((ballRect->y < 0) || (ballRect->y + ballRect->h > (SCREEN_HEIGHT - floorOffset)))
+    if((bRect->y < 0) || (bRect->y + bRect->h > (SCREEN_HEIGHT - floorOffset)))
     {
         /* Change direction */
         ac = ac * -1;
-        printf("ac = %d\n", ac);
         
         /* Reverse acceleration */
         directionY = directionY * -1;
 
+    }
+}
+
+/* Sim smooth movement */
+void move(SDL_Rect *bRect)
+{
+    bRect->x += velX;
+
+    /* Detect wall */
+    if((bRect->x < 0 ) || (bRect->x + bRect->w > SCREEN_WIDTH)){
+        bRect->x -= velX;
+    }
+
+    bRect->y += velY;
+
+    /* Detect floor/ceiling */
+    if(( bRect->y < 0 ) || (bRect->y + bRect->h > SCREEN_HEIGHT )){
+        bRect->y -= velY;
     }
 }
